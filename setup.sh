72c0776c8489e9a9875beaa8d7618cb9e6693826
AathmanT <35717580+AathmanT@users.noreply.github.com>
2020-08-31 01:12:12 +0530

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,6 @@
+sudo apt-get update
+sudo apt-get install git
+#git clone https://github.com/AathmanT/GSOMnal-neural-network-with-attention.git
+sudo apt-get install python3-pip
+cd GSOMnal-neural-network-with-attention/
+pip3 install -r requirements.txt
